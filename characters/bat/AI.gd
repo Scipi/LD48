b8431da99af3c71c1e0d2a4eb618fc31826aa461
Jophire <36978900+Jophire@users.noreply.github.com>
2021-04-24 20:21:26 -0400

    Update AI.gd

--- characters/bat/AI.gd
+++ characters/bat/AI.gd
@@ -19,18 +19,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var senses = get_parent().get_node("Senses")
 	
 	var goal = Vector3(0,2,0)#nav.get_closest_point(Vector3(0,2,0))
-	
 	if player:
-		goal = player.get_global_transform().origin+Vector3.UP*4
-	var senses = get_parent().get_node("Senses")
-	var position = self.get_global_transform().origin
-	var trans = self.get_parent().get_global_transform().looking_at(goal,Vector3.UP)
-	var next_trans = self.get_parent_spatial().get_global_transform().interpolate_with(trans,delta*rng.randf_range(-4,6))
-	self.get_parent_spatial().set_global_transform(next_trans)
+		goal = player.get_global_transform().origin+Vector3.UP*3*rng.randf_range(-2.5,5)
+	
+	var cur_trans = self.get_parent_spatial().get_global_transform()
+	var final_trans = cur_trans.looking_at(goal,Vector3.UP)
+	var look_trans = self.get_parent_spatial().get_global_transform().interpolate_with(final_trans,delta*rng.randf_range(-4,6))
+	self.get_parent_spatial().set_global_transform(look_trans)
 	
 	#Bat does not fly up or down, not sure.
-	var move_vec =Vector3(0,-next_trans.basis.z.length(),-next_trans.basis.z.length())
+	var move_vec =Vector3(0,-look_trans.basis.z.y,-look_trans.basis.z.length())
 
 	character_mover.set_move_vec(move_vec)
